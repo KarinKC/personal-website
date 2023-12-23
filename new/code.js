d3280460aa1532b17c8a31bcd2d32b6d52bcb224
Karin Kay <karin.kay9@gmail.com>
2023-12-23 19:14:16 +0000

    Reduce navbar size and fix theme color update when expanding navbar

--- new/code.js
+++ new/code.js
@@ -7,8 +7,19 @@ function toggleClass(cssClass, elements) {
     for (var i = 0; i < elements.length; i++) {
         if (elements[i].classList.contains(cssClass)) {
             elements[i].classList.remove(cssClass);
+            changeStatusBarColor(false)
         } else {
             elements[i].classList.add(cssClass);
+            changeStatusBarColor(true)
         }
     }
+}
+
+function changeStatusBarColor(isNavbarExpanded) {
+    if (isNavbarExpanded) {
+        color = getComputedStyle(document.documentElement).getPropertyValue("--color-brown")
+    } else {
+        color = "white"
+    }
+    document.querySelector('meta[name="theme-color"]').setAttribute('content',  color);
 }
